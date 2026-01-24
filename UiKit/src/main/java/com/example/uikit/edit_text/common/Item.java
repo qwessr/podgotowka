6ba87fea854bd4b74945876e485dcf07cb308479
Iliya12 <tankist228121234567890@gmail.com>
2026-01-24 16:26:43 +0500

    прописан Card без проверки

---
+++ UiKit/src/main/java/com/example/uikit/edit_text/common/Item.java
@@ -0,0 +1,18 @@
+package com.example.uikit.edit_text.common;
+
+public class Item
+{
+    public String Name;
+
+    public String Category;
+
+    public Integer Price;
+
+    public Item(String name, String category, Integer price)
+    {
+        Name = name;
+        Category = category;
+        Price = price;
+    }
+
+}
